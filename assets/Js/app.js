dbb3a5099dfff731af2be2185a97a0aadf411468
shahreeyarrahman <shahreeyar.cnits@gmail.com>
2024-08-18 21:50:17 +0600

    fixing blog section

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -4,3 +4,38 @@ headerBtnEffect.addEventListener("click", function(){
 })
 
 // counter //
+
+
+
+//  START CARD BLOG SLIDER //  
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        stagePadding: 10,
+        margin: 30,
+        nav: true,
+        navText : [
+            '<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>',
+            '<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'
+        ],
+        autoplay: true,                 // Enable auto-slide
+        autoplayTimeout: 3000,          // Delay in milliseconds (3000 ms = 3 seconds)
+        autoplayHoverPause: true,       // Pause on hover
+        responsive:{
+            0: {
+                items: 1
+            },
+            640: {
+                items: 2
+            },
+            960: {
+                items: 3
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
+});
+//  END  CARD BLOG SLIDER //  
+
